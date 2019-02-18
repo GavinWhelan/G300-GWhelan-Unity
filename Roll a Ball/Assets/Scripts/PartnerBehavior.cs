8ed1df48cbe6e55832fe9965bcc90616ecba780e
Gavin Whelan <gavinpwhelan@gmail.com>
2019-02-18 01:57:50 -0500

    Completed Partner Behavior script, including forcing partners into an orbit around the Player

--- Roll a Ball/Assets/Scripts/PartnerBehavior.cs
+++ Roll a Ball/Assets/Scripts/PartnerBehavior.cs
@@ -17,10 +17,23 @@ public class PartnerBehavior : MonoBehaviour
     {
         if (linked == true)
         {
+            /*
             float moveHorizontal = Input.GetAxis("Horizontal");
             float moveVertical = Input.GetAxis("Vertical");
 
             Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            */
+
+            Vector3 movement;
+
+            if (Vector3.Distance(GameObject.Find("Player").transform.position, GetComponent<Transform>().position) >= 2.0f)
+            {
+                movement = GameObject.Find("Player").transform.position - GetComponent<Transform>().position;
+            }
+            else
+            {
+                movement = GetComponent<Transform>().position - GameObject.Find("Player").transform.position;
+            }
 
             GetComponent<Rigidbody>().AddForce(movement * speed);
         }
@@ -29,7 +42,7 @@ public class PartnerBehavior : MonoBehaviour
     // Checks whether the player has collided with the partner
     private void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.name == "Player")
+        if (col.gameObject.name == "Player" || col.gameObject.tag == "Partner")
         {
             linked = true;
         }
